2b8487476a330c2bd1a578eefe6629b307b5162f
SL <samanthl795@gmail.com>
2021-06-30 11:24:56 -0700

    Add form elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,11 @@ var timerInterval = setInterval(function() {
     var timeAndDate = moment();
     currentTimeAndDateEl.text(timeAndDate.format("MMM D, YYYY [at] h:mm:ss a") );
   }, 1000);
+
+// Datepicker widget
+$(function () {
+    $('#due-date').datepicker({
+      changeMonth: true,
+      changeYear: true,
+    });
+  });  
